82c8ea4e817fd35ba820154165e04523c1cfd5d7
Seth Young <sethyoung1@gmx.com>
2014-12-07 17:37:36 -0500

    .R and .png plot files added

---
+++ plot2.R
@@ -0,0 +1,26 @@
+require("dplyr")
+
+## read the data file up to line 70,000 (the data we need ends before this point) 
+furl <- "household_power_consumption.txt"
+house <- read.csv(furl, sep = ";", nrows = 70000, header = TRUE, na.strings = "?",
+                  colClasses = c("character",
+                                 "character",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric")
+)
+## select data for the 2 specified dates
+house <- filter(house, Date == "1/2/2007" | Date == "2/2/2007")
+## paste date+time together and create a new Date_time column of class posixct
+x <- strptime(paste(house$Date, house$Time), format="%d/%m/%Y %H:%M:%S")
+hpc <- mutate(house, Date_time = as.POSIXct(x))
+
+## create plot in a png file
+png(file = "plot2.png", width = 480, height = 480)
+with(hpc, plot(Global_active_power ~ Date_time, type = "l", xlab = "",
+               ylab = "Global Active Power (kilowatts)"))
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,42 @@
+require("dplyr")
+
+## read the data file up to line 70,000 (the data we need ends before this point) 
+furl <- "household_power_consumption.txt"
+house <- read.csv(furl, sep = ";", nrows = 70000, header = TRUE, na.strings = "?",
+                  colClasses = c("character",
+                                 "character",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric",
+                                 "numeric")
+)
+## select data for the 2 specified dates
+house <- filter(house, Date == "1/2/2007" | Date == "2/2/2007")
+## paste date+time together and create a new Date_time column of class posixct
+x <- strptime(paste(house$Date, house$Time), format="%d/%m/%Y %H:%M:%S")
+hpc <- mutate(house, Date_time = as.POSIXct(x))
+
+## create plots in a png file
+png(file = "plot4.png", width = 480, height = 480)
+par(mfcol = c(2,2))
+
+with(hpc, plot(Global_active_power ~ Date_time, type = "l", xlab = "",
+               ylab = "Global Active Power"))
+
+with(hpc, plot(Sub_metering_1 ~ Date_time, type = "l", xlab = "",
+               ylab = "Energy sub metering"))
+lines(x = hpc$Date_time, y = hpc$Sub_metering_2, col = "red")
+lines(x = hpc$Date_time, y = hpc$Sub_metering_3, col = "blue")
+legend("topright", lty = 1, bty = "n", cex = 0.9, col = c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+with(hpc, plot(Voltage ~ Date_time, type = "l", xlab = "datetime",
+               ylab = "Voltage"))
+
+with(hpc, plot(Global_reactive_power ~ Date_time, type = "l", xlab = "datetime",
+               ylab = "Global_reactive_power"))
+
+dev.off()
